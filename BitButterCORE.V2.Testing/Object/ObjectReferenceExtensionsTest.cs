18fa13d3d21681a0bb7f820245414b414b8383ef
YPZhou <yunpeng_zhou@163.com>
2024-09-21 15:28:26 +0800

    Add extension method IsValid() for IObjectReference

---
+++ BitButterCORE.V2.Testing/Object/ObjectReferenceExtensionsTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using BitButterCORE.V2.Object;
+
+namespace BitButterCORE.V2.Testing
+{
+	[TestFixture]
+	public class ObjectReferenceExtensionsTest
+	{
+		[SetUp]
+		public void Setup()
+		{
+			ObjectFactory.Instance.RemoveAll();
+			ObjectFactory.Instance.ResetIDFountains();
+		}
+
+		[Test]
+		public void TestIsValid()
+		{
+			var nullReference = default(IObjectReference);
+			Assert.That(nullReference.IsValid(), Is.EqualTo(false), "Null reference is not valid");
+
+			var invalidReference = ObjectFactory.Instance.Create<DummyObject>();
+			ObjectFactory.Instance.Remove(invalidReference);
+			Assert.That(invalidReference.IsValid(), Is.EqualTo(false), "Reference without object is not valid");
+
+			var objectReference = ObjectFactory.Instance.Create<DummyObject>();
+			Assert.That(objectReference.IsValid(), Is.EqualTo(true), "Reference with object is valid");
+		}
+	}
+}
---
+++ BitButterCORE.V2/Object/ObjectReferenceExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace BitButterCORE.V2.Object
+{
+	public static class ObjectReferenceExtensions
+	{
+		public static bool IsValid(this IObjectReference reference)
+		{
+			return reference != null && reference.IsValid;
+		}
+	}
+}
